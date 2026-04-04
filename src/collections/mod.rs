6e0fd3e3e1df5b69da5f757b88ada7c5740f7553
MarelGuy <irertnuc.loris@gmail.com>
2026-04-04 12:31:22 +0200

    added general Purpose struct with String conversion

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -8,6 +8,25 @@ pub use channels::{ChannelPurpose, Channels};
 pub use roles::{RolePurpose, Roles};
 pub use users::Users;
 
+use crate::BoxError;
+
 #[derive(Schema)]
 #[schema(name = "hesper-schema", collections = [Users, Channels, Roles])]
 pub struct HesperSchema;
+
+pub enum Purpose {
+    RolePurpose,
+    ChannelPurpose,
+}
+
+impl TryFrom<&String> for Purpose {
+    type Error = BoxError;
+
+    fn try_from(value: &String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "RolePurpose" => Ok(Purpose::RolePurpose),
+            "ChannelPurpose" => Ok(Purpose::ChannelPurpose),
+            _ => Err("Unkown Purpose".into()),
+        }
+    }
+}
